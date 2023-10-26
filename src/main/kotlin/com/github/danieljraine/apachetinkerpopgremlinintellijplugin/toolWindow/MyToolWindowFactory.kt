94fcee32accad2518a56cae04da025430b00c44e
GitHub Action <action@github.com>
2023-10-26 01:16:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/danieljraine/apachetinkerpopgremlinintellijplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/danieljraine/apachetinkerpopgremlinintellijplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.danieljraine.apachetinkerpopgremlinintellijplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.danieljraine.apachetinkerpopgremlinintellijplugin.MyBundle
+import com.github.danieljraine.apachetinkerpopgremlinintellijplugin.services.MyProjectService
 import javax.swing.JButton
 
 
